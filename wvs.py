9379734e3512e4dc46c9af1d6b8f44b9a5d13ad1
crelke0 <clarke.elke.mail@gmail.com>
2022-02-07 21:37:12 -0600

    added KDNode class

---
+++ wvs.py
@@ -0,0 +1,16 @@
+from PIL import Image
+from numpy import left_shift
+
+class KDNode:
+  def __init__(self, pos, axis, left, right):
+    self.pos = pos
+    self.axis = axis
+    self.left = left
+    self.right = right
+      
+
+def main():
+  pass
+
+if __name__ == "__main__":
+  main()
