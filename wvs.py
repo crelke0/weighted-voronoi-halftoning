4e7c46c56c2e1d5e2eda207bf969598342030ac3
Clarke Elke <95830459+crelke0@users.noreply.github.com>
2022-02-23 09:54:20 -0600

    Update wvs.py

--- wvs.py
+++ wvs.py
@@ -37,13 +37,11 @@ class KDNode:
   # axis {int} which axis the node splits space (0 splits x-axis, 1 splits y-axis)
   # left {KDNode} the left child node
   # right {KDNode} the right child node
-  def __init__(self, pos, axis, left, right):
+  def __init__(self, pos, left, right):
     self.pos = pos
-    self.axis = axis
     self.left = left
     self.right = right
-    g = id(self)*51234517%256
-    self.color = g
+    self.color = id(self)*51234517%256
 
   # creates tree from list of points
   # points {list}
@@ -80,52 +78,17 @@ class KDNode:
     left = KDNode.create_tree(left_of_median, depth + 1)
     right = KDNode.create_tree(right_of_median, depth + 1)
 
-    return KDNode(median, axis, left, right)
+    return KDNode(median, left, right)
     
   # finds the nearest neighbor of a point
   # point {Vector} the point to find the nearest neighbor of
   # return {tuple} (nearest neighbor {KDNode}, squared distance {float})
-  def find_nn(self, point):
-    smallest = (self.pos.x - point.x)**2 + (self.pos.y - point.y)**2
-    nearest_neighbor = self
-    if self.left == None and self.right == None:
-      return nearest_neighbor, smallest
-
-    # function returns x or y depending on this nodes axis
-    axis_coord = None
-    if self.axis == 0:
-      axis_coord = lambda a: a.x
-    else:
-      axis_coord = lambda a: a.y
-
-    # determine which subtree should be checked first
-    first = self.left
-    other = self.right
-    if axis_coord(point) > axis_coord(self.pos):
-      first = self.right
-      other = self.left
-    if first == None:
-      first, other = other, first
-
-    first_nn, first_smallest = first.find_nn(point)
-    if first_smallest < smallest:
-      smallest = first_smallest
-      nearest_neighbor = first_nn
-
-    # function for finding the distance to this nodes line that splits space
-    axis_distance = None
-    if self.axis == 0:
-      axis_distance = lambda a, b: abs(a.y - b.y)
-    else:
-      axis_distance = lambda a, b: abs(a.x - b.x)
-
-    if first_smallest > axis_distance(self.pos, first.pos) and other != None:
-      other_nn, other_smallest = other.find_nn(point)
-      if other_smallest < smallest:
-        smallest = other_smallest
-        nearest_neighbor = other_nn
-
-    return nearest_neighbor, smallest
+  def find_nn(self, point, depth):
+    find_sq_dist = lambda a, b: (a.pos.x - b.pos.x)**2 + (a.pos.y - b.pos.y)**2
+    self_sq_dist = find_sq_dist(self.pos, point)
+    if self.left == None and self.left == None:
+      return self_sq_dist, self
+         
 
 def main():
   width = 500
